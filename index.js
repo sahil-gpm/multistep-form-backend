7d680654ecd1e2e5dc5448785c19e3183930d071
Sahil Chavan <142314251+sahilchavan94@users.noreply.github.com>
2024-08-09 23:21:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const cors = require("cors");
 const dotenv = require('dotenv')
 const connectToMongo = require('./db/mongo')
 
+dotenv.config();
+
 const app = express();
 
 // adding middlewares
 app.use(
   cors({
-    origin:"*",
+    origin:process.env.FRONTEND,
   })
 );
 
